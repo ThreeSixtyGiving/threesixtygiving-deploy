7f17e8dcb85da44500d6e9bb8a1eaded032be5d2
Michael Wood <michael.wood@opendataservices.coop>
2020-12-07 14:37:54 +0000

    updateToMain: If private repos aren't present clone them

--- updateToMain.sh
+++ updateToMain.sh
@@ -6,12 +6,20 @@ git checkout main
 git pull
 
 echo "--------------------- PILLAR / PRIVATE"
+if [ ! -f "pillar/private" ]; then
+  git clone git@github.com:ThreeSixtyGiving/threesixtygiving-deploy-pillar-private.git pillar/private
+fi
+
 cd pillar/private
 git checkout main
 git pull
 cd ../..
 
 echo "--------------------- SALT / PRIVATE"
+if [ ! -f "salt/private" ]; then
+  git clone git@github.com:ThreeSixtyGiving/threesixtygiving-deploy-salt-private.git salt/private
+fi
+
 cd salt/private
 git checkout main
 git pull
